0c7510528bdf8a36197a8a0340e994f98f6bbb44
Carl Cassar <carlcassar@gmail.com>
2021-02-08 18:28:26 +0000

    Remove unused function

--- resources/views/components/tag-cloud.blade.php
+++ resources/views/components/tag-cloud.blade.php
@@ -4,7 +4,7 @@
            class="flex items-center space-x-2 px-4 py-1 border hover:border-orange-600 transition-color duration-200 rounded-lg cursor-pointer mb-2 {{ isset($currentTag) && $currentTag->name === $tag->name ? 'border-orange-600' : '' }}"
         >
             <i class="bi-circle" style="color: {{ $tag->colour }}; font-size:.5em"></i>
-            <span>{{ Str::title($tag->name) }}</span>
+            <span>{{ $tag->name }}</span>
         </a>
     @endforeach
 </div>
