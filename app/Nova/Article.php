4cd4c2e220aee82ad066ce8135b4378d12142a80
Carl Cassar <carlcassar@gmail.com>
2021-02-09 15:28:45 +0000

    Continue Laravel Nova Installation

---
+++ app/Nova/Article.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Nova;
+
+use Illuminate\Http\Request;
+use Laravel\Nova\Fields\ID;
+use Laravel\Nova\Fields\Text;
+use Laravel\Nova\Http\Requests\NovaRequest;
+
+class Article extends Resource
+{
+    public static $model = \App\Models\Article::class;
+
+    public static $title = 'id';
+
+    public static $search = [
+        'id',
+        'title',
+        'body',
+    ];
+
+    public function fields(Request $request)
+    {
+        return [
+            ID::make( 'id')->sortable(),
+
+            Text::make('title'),
+        ];
+    }
+
+    /**
+     * Get the cards available for the request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function cards(Request $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the filters available for the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function filters(Request $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the lenses available for the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function lenses(Request $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the actions available for the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function actions(Request $request)
+    {
+        return [];
+    }
+}
