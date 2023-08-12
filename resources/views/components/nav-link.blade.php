4c47c10d2625066f9b8fa31010140fb186c76090
Carl Cassar <carlcassar@gmail.com>
2023-08-12 17:43:09 +0100

    Improve navigation styles

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -2,10 +2,12 @@
 
 @php
 $classes = ($active ?? false)
-            ? 'inline-flex items-center px-1 pt-1 border-b-2 border-indigo-400 dark:border-indigo-600 text-sm font-medium leading-5 text-gray-900 dark:text-gray-100 focus:outline-none focus:border-indigo-700 transition duration-150 ease-in-out'
-            : 'inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-300 hover:border-gray-300 dark:hover:border-gray-700 focus:outline-none focus:text-gray-700 dark:focus:text-gray-300 focus:border-gray-300 dark:focus:border-gray-700 transition duration-150 ease-in-out';
+            ? 'px-4 py-2 rounded-lg hover:bg-gray-200 hover:dark:bg-gray-800 bg-gray-50 dark:bg-gray-950 text-gray-800 hover:text-orange-700 dark:text-gray-100 transition duration-200'
+            : 'px-4 py-2 rounded-lg hover:bg-gray-200 hover:dark:bg-gray-800 text-gray-500 hover:text-orange-700 dark:text-gray-100 transition duration-200';
 @endphp
 
-<a {{ $attributes->merge(['class' => $classes]) }}>
+<a class="uppercase cursor-default inline-flex items-center px-1 pt-1 text-sm tracking-widest font-semibold leading-5 focus:outline-none focus:border-indigo-700 transition duration-150 ease-in-out">
+    <div {{ $attributes->merge(['class' => $classes]) }}>
     {{ $slot }}
+    </div>
 </a>
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,4 +1,4 @@
-<nav x-data="{ open: false }" class="bg-white dark:bg-gray-800 border-b border-gray-100 dark:border-gray-700">
+<nav x-data="{ open: false }">
     <!-- Primary Navigation Menu -->
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
@@ -8,12 +8,21 @@
                     <a href="{{ route('dashboard') }}">
                         <x-application-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />
                     </a>
+
+                    <a href="{{ route('dashboard') }}"
+                          class="ml-4 inline-flex items-center text-2xl font-semibold transition-all duration-200 cursor-pointer dark:text-white hover:text-orange-700">
+                    carlcassar.com
+                    </a>
                 </div>
 
                 <!-- Navigation Links -->
-                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                <div class="hidden space-x-2 sm:-my-px sm:ml-10 sm:flex">
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
+                    </x-nav-link>
+
+                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboards')">
+                        {{ __('Another') }}
                     </x-nav-link>
                 </div>
             </div>
@@ -22,7 +31,7 @@
             <div class="hidden sm:flex sm:items-center sm:ml-6">
                 <x-dropdown align="right" width="48">
                     <x-slot name="trigger">
-                        <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150">
+                        <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150">
                             <div>{{ Auth::user()->name }}</div>
 
                             <div class="ml-1">
