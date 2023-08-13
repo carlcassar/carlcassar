36881f46d5e6e07b65d9b6929d8fe7c7fb42e701
Carl Cassar <carlcassar@gmail.com>
2023-08-13 22:12:29 +0100

    Add Tags and Years Widgets

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,10 @@
 
 namespace App\Providers;
 
+use App\Models\Article;
+use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
+use Str;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +22,23 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        View::share('tags', $this->allTags());
+        View::share('years', $this->allYears());
+    }
+
+    public function allTags()
+    {
+        $tags = Article::get('tags')->map(fn ($article) => $article->tags)->join(', ');
+
+        return Str::of($tags)->explode(', ')->unique();
+    }
+
+    public function allYears()
+    {
+        return $years = Article::get('created_at')
+            ->map(fn ($article) => $article->created_at)
+            ->filter()
+            ->map(fn ($date) => $date->format('Y'))
+            ->unique();
     }
 }
