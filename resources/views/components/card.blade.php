ad7fc55fbc841a764ad0660bf8c9cdef099b7917
Carl Cassar <carlcassar@gmail.com>
2023-08-13 10:47:22 +0100

    Give the navigation bar some breathing room

---
+++ resources/views/components/card.blade.php
@@ -0,0 +1,8 @@
+    <div {{ $attributes->merge(['class' => 'bg-white dark:bg-gray-900 overflow-hidden sm:border dark:border-gray-900 sm:rounded-xl']) }}>
+        @if(isset($title))
+        <div class="pb-2 text-sm font-semibold uppercase text-gray-800 dark:text-white">
+            {{ $title }}
+        </div>
+        @endif
+        {{ $slot }}
+    </div>
--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -6,7 +6,7 @@ $classes = ($active ?? false)
             : 'px-4 py-2 rounded-lg hover:bg-gray-200 hover:dark:bg-gray-800 text-gray-500 hover:text-orange-700 dark:text-gray-100 transition duration-200';
 @endphp
 
-<a {{ $attributes->merge(['class' => 'uppercase cursor-default inline-flex items-center px-1 pt-1 text-sm tracking-widest font-semibold leading-5 focus:outline-none focus:border-indigo-700 transition duration-150 ease-in-out']) }}>
+<a {{ $attributes->merge(['class' => 'hidden md:block uppercase cursor-default inline-flex items-center px-1 pt-1 text-sm tracking-widest font-semibold leading-5 focus:outline-none focus:border-indigo-700 transition duration-150 ease-in-out']) }}>
     <div class="{{ $classes }}">
     {{ $slot }}
     </div>
