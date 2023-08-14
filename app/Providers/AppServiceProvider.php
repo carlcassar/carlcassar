33ce1d9dd031a56e7be3aebc025b959846419c14
Carl Cassar <carlcassar@gmail.com>
2023-08-14 20:38:39 +0100

    Remove unused imports

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,10 +2,7 @@
 
 namespace App\Providers;
 
-use App\Models\Article;
-use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
-use Str;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -22,32 +19,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        View::share('recents', $this->recents());
-        View::share('tags', $this->allTags());
-        View::share('years', $this->allYears());
-    }
-
-    public function allTags()
-    {
-        $tags = Article::get('tags')->map(fn ($article) => $article->tags)->join(', ');
-
-        return Str::of($tags)->explode(', ')->unique();
-    }
-
-    public function allYears()
-    {
-        return Article::get('created_at')
-            ->map(fn ($article) => $article->created_at)
-            ->filter()
-            ->map(fn ($date) => $date->format('Y'))
-            ->unique();
-    }
-
-    public function recents()
-    {
-        return Article::query()
-            ->orderByDesc('published_at')
-            ->limit(3)
-            ->get();
+        //
     }
 }
