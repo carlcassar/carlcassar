85f073e17806513221231ed7c8a90051a834f88c
Carl Cassar <carlcassar@gmail.com>
2023-08-17 11:20:37 +0100

    Add code syntax highlighting

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,21 @@
 import './bootstrap';
 
+// resources/js/prismjs.js
+import Prism from 'prismjs';
+
+// You can include specific languages if you know which ones you'll use
+import 'prismjs/components/prism-markup.min.js';
+import 'prismjs/components/prism-markup-templating.min.js'
+import 'prismjs/components/prism-php.min.js';
+import 'prismjs/components/prism-javascript.min.js';
+import 'prismjs/components/prism-swift.min.js';
+import 'prismjs/components/prism-bash.min.js';
+import 'prismjs/components/prism-wolfram.min.js';
+
+// Initialize Prism
+Prism.highlightAll();
+
+
 // import Alpine from 'alpinejs';
 
 // window.Alpine = Alpine;
