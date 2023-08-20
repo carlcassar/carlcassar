011e42c6265b2565a40ab06842b2dd5ad0cd0704
Carl Cassar <carlcassar@gmail.com>
2023-08-20 13:05:36 +0100

    Add aria label to logo

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -6,7 +6,7 @@
             <!-- Navigation Links -->
             <div class="sm:-my-px sm:flex">
                 <div class="shrink-0 flex items-center">
-                    <a href="{{ route('home') }}" alt="Home">
+                    <a href="{{ route('home') }}" alt="Go Home" aria-label="Go Home">
                         <x-application-logo class="block h-10 w-auto fill-current text-gray-800 dark:text-gray-200"/>
                     </a>
 
@@ -76,7 +76,7 @@
             <div class="-mr-2 -mt-2 flex items-center sm:hidden">
                 <button @click="open = ! open"
                         class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 dark:text-gray-500 hover:text-gray-500 dark:hover:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-900 focus:outline-none focus:bg-gray-100 dark:focus:bg-gray-900 focus:text-gray-500 dark:focus:text-gray-400 transition duration-150 ease-in-out"
-                        aria-label="Menu">>
+                        aria-label="Menu">
                     <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
                         <path :class="{'hidden': open, 'inline-flex': ! open }" class="inline-flex"
                               stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
