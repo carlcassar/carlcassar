fa5d690017c089b555a3ba5118fe627785ee0492
Carl Cassar <carlcassar@gmail.com>
2023-08-26 18:52:28 +0100

    Resolve issue with TOC linking

--- app/Console/Commands/MarkdownImport.php
+++ app/Console/Commands/MarkdownImport.php
@@ -142,6 +142,7 @@ class MarkdownImport extends Command
                 'html_class' => 'no-underline mr-2 text-gray-500',
                 'aria_hidden' => false,
                 'id_prefix' => '',
+                'fragment_prefix' => '',
             ],
         ]);
 
