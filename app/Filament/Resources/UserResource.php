63febc549c708239f8a72d5b03a042b9b3a5ea2e
Carl Cassar <carlcassar@gmail.com>
2023-08-27 22:20:41 +0100

    Fix issue where users can't be saved without a password

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -30,7 +30,6 @@ class UserResource extends Resource
                 Forms\Components\DateTimePicker::make('email_verified_at'),
                 Forms\Components\TextInput::make('password')
                     ->password()
-                    ->required()
                     ->maxLength(255),
                 Forms\Components\Toggle::make('is_admin')
                     ->required(),
