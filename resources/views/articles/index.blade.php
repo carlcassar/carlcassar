18e4c3e01bfad4b020490c91dd761080a57bb8cc
Carl Cassar <carlcassar@gmail.com>
2023-08-29 00:52:55 +0100

    Fix issue with article filter labels in dark mode

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -18,15 +18,15 @@
         <h1 class="font-extrabold text-3xl text-gray-800 dark:text-gray-200 leading-tight">
             {{ __('Articles') }}
             @if($tag)
-                <span class="text-sm uppercase text-gray-600">
+                <span class="text-sm uppercase text-gray-600 dark:text-gray-500">
                 with the tag
-                <span class="text-black">{{ $tag }}</span>
+                <span class="text-black dark:text-gray-300">{{ $tag }}</span>
             </span>
             @endif
             @if($year)
-                <span class="text-sm uppercase text-gray-600">
+                <span class="text-sm uppercase text-gray-600 dark:text-gray-500">
                 in the year
-                <span class="text-black">{{ $year }}</span>
+                <span class="text-black dark:text-gray-300">{{ $year }}</span>
             </span>
             @endif
         </h1>
