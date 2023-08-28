d0dd51efa989b2d93068e0ac0194a3ef7a617dd7
Carl Cassar <carlcassar@gmail.com>
2023-08-28 23:26:34 +0100

    Add dashboard tests

---
+++ tests/Feature/DashboardTest.php
@@ -0,0 +1,22 @@
+<?php
+
+use App\Models\User;
+
+use Symfony\Component\HttpFoundation\Response;
+use function Pest\Laravel\actingAs;
+use function Pest\Laravel\get;
+
+it('is displayed when a user is logged in', function () {
+    actingAs(User::factory()->create());
+
+    $response = get('/dashboard');
+
+    $response->assertStatus(Response::HTTP_OK);
+});
+
+it('cannot be seen by a guest', function () {
+    $response = get('/dashboard');
+
+    $response->assertStatus(Response::HTTP_FOUND);
+    $response->assertRedirect(route('login'));
+});
