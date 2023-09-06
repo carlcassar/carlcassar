4e4fafd8adfa4ce37caa2d4b6e77066f572b230d
Carl Cassar <carlcassar@gmail.com>
2023-09-06 14:03:41 +0100

    Modify notification settings text

--- resources/views/livewire/notification-settings.blade.php
+++ resources/views/livewire/notification-settings.blade.php
@@ -1,8 +1,7 @@
 <div>
     <section id="notifications">
         <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
-            Choose which notifications you would like to receive. Transactional email relating to your account will
-            be sent sparingly whenever necessary.
+            Choose which email notifications you would like to receive.
         </p>
 
         <form wire:submit="create" class="mt-6 space-y-6">
