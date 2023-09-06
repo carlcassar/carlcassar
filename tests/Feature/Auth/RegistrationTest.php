438555b4c1809bbb62f21a3db153c98d674361da
Carl Cassar <carlcassar@gmail.com>
2023-09-06 10:39:55 +0100

    Test that welcome email is sent to new users

--- tests/Feature/Auth/RegistrationTest.php
+++ tests/Feature/Auth/RegistrationTest.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Models\User;
+use App\Notifications\Welcome;
 use App\Providers\RouteServiceProvider;
+use Illuminate\Auth\Events\Registered;
 
 test('registration screen can be rendered', function () {
     $response = $this->get('/register');
@@ -18,4 +21,38 @@ test('new users can register', function () {
 
     $this->assertAuthenticated();
     $response->assertRedirect(RouteServiceProvider::HOME);
+
+    $this->assertCount(1, User::all());
+});
+
+test('a registered event is dispatched when a user registers', function () {
+    Event::fake();
+
+    $this->post('/register', [
+        'name' => 'Test User',
+        'email' => 'test@example.com',
+        'password' => 'password',
+        'password_confirmation' => 'password',
+    ]);
+
+    Event::assertDispatched(Registered::class);
+});
+
+test('a user is sent a welcome notification when they register', function () {
+    Notification::fake();
+
+    $this->post('/register', [
+        'name' => 'Test User',
+        'email' => 'test@example.com',
+        'password' => 'password',
+        'password_confirmation' => 'password',
+    ]);
+
+    Notification::assertSentTo($user = User::first(), Welcome::class,
+        function (Welcome $notification, $channels) use ($user) {
+            $mailable = $notification->toMail($user);
+
+            return $channels == ['mail', 'database'] &&
+                $mailable->subject = 'Welcome';
+        });
 });
