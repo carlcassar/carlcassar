63acd07712e4cad3b89529a6827081e3f625a62e
Carl Cassar <carlcassar@gmail.com>
2024-03-24 19:08:16 +0000

    Remove old markdown importer and add tests for importing markdown files

---
+++ tests/Feature/ImportMarkdownTest.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Support\Carbon;
+
+it('says that the markdown has been imported', function () {
+    $this->artisan('markdown-tools:process')->expectsOutput('Your schemes have all been processed successfully.');
+
+    $this->assertDatabaseHas('articles', [
+        'title' => 'Tidying Tips',
+        'slug' => 'tidying-tips',
+        'tags' => ['php'],
+        'published_at' => Carbon::make('2018-08-10 19:19:00'),
+        'created_at' => Carbon::make('2018-08-10 19:19:00'),
+        'updated_at' => Carbon::make('2018-08-10 19:19:00'),
+        'deleted_at' => null,
+    ]);
+});
