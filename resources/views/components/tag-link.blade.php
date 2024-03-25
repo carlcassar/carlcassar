48d170df2c3b2edef378d99af3aa0f3f280952c2
Carl Cassar <carlcassar@gmail.com>
2024-03-25 23:45:39 +0000

    Improve tag button style

--- resources/views/components/tag-link.blade.php
+++ resources/views/components/tag-link.blade.php
@@ -3,6 +3,6 @@
 ])
 
 <a href="{{route('articles.index', ['tag' => $tag]) }}"
-   class="text-center capitalize px-2 py-0.5 text-sm tracking-wide rounded-md border border-gray-200 dark:border-gray-700 text-gray-700 dark:text-gray-200 hover:border-orange-600 transition-colors duration-200">
+   class="text-center capitalize px-2 py-0.5 text-sm tracking-wide rounded-md border border-gray-200 dark:border-gray-700 text-gray-700 dark:text-gray-200 hover:border-orange-600 transition-all duration-200 hover:-translate-y-0.5 hover:font-semibold">
     {{ $tag }}
 </a>
