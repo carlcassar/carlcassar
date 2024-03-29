b845557106a166c9156a6c89dc9a9cd5cd8b3539
Carl Cassar <carlcassar@gmail.com>
2024-03-29 17:01:05 +0000

    Resolve accessibility issue on tags links

--- resources/views/components/tags.blade.php
+++ resources/views/components/tags.blade.php
@@ -7,10 +7,13 @@
         @foreach($tags as $tag => $count)
             <li class="flex items-center justify-between space-x-2 my-1">
                 <x-link class="no-underline" href="{{route('tags.show', $tag)}}">{{ $tag }}</x-link>
-                <div class="text-xs h-5 w-5 bg-gray-100 dark:bg-gray-700 rounded-full flex justify-center items-center">{{ $count }}</div>
+                <div
+                    class="text-xs h-5 w-5 bg-gray-100 dark:bg-gray-700 rounded-full flex justify-center items-center">{{ $count }}</div>
             </li>
         @endforeach
 
-        <x-link class="block mt-4" :href="route('tags.index')">More Tags...</x-link>
+        <li>
+            <x-link class="block mt-4" :href="route('tags.index')">More Tags...</x-link>
+        </li>
     </ul>
 </x-card>
