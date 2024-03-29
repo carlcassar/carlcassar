ccf3c054ea2960a3af79f2c39399d2b3111f257a
Carl Cassar <carlcassar@gmail.com>
2024-03-29 16:06:48 +0000

    Make article pages canonical

--- resources/views/components/meta.blade.php
+++ resources/views/components/meta.blade.php
@@ -17,7 +17,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta name="csrf-token" content="{{ csrf_token() }}">
 
-<link rel="canonical" href="{{ request()->fullUrlWithoutQuery('page') }}" />
+<link rel="canonical" href="{{ request()->fullUrl() }}" />
 
 <title>{{$title}}</title>
 <meta name="description" content="{!! $description !!}">
